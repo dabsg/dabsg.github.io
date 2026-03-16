037007ab96408d87404fcc71165c1e9e9b3e4f74
dabsg <115629722+dabsg@users.noreply.github.com>
2026-03-16 15:57:19 +0100

    test

--- pwa/app.js
+++ pwa/app.js
@@ -11,3 +11,10 @@ if ("serviceWorker" in navigator) {
   });
 
 }
+window.addEventListener("deviceorientation", event => {
+
+  document.getElementById("alpha").textContent = event.alpha.toFixed(2);
+  document.getElementById("beta").textContent = event.beta.toFixed(2);
+  document.getElementById("gamma").textContent = event.gamma.toFixed(2);
+
+});
